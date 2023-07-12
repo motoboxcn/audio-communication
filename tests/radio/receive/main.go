c01ed976915cb09a763788e3840a3d5bff329f38
mikas <zhoushoujianwork@163.com>
2023-07-12 16:08:16 +0100

    chang

--- tests/radio/receive/main.go
+++ tests/radio/receive/main.go
@@ -17,25 +17,24 @@ func main() {
 
 	pin := rpio.Pin(20)
 	pin.Input()
-
-	data := make([]byte, 4)
-
 	for {
-		for i := 0; i < len(data); i++ {
-			for j := 0; j < 8; j++ {
-				time.Sleep(time.Millisecond * 1)
-				data[i] <<= 1
-				data[i] |= uint8(pin.Read())
-			}
-		}
-
-		parseData(data)
-
-		time.Sleep(time.Millisecond * 500)
+		receiveData := receiveCode(pin)
+		fmt.Printf("Received data: %v\n", receiveData)
+		time.Sleep(time.Millisecond * 100)
 	}
+
 }
 
-func parseData(data []byte) {
-	value := uint32(data[0])<<24 | uint32(data[1])<<16 | uint32(data[2])<<8 | uint32(data[3])
-	fmt.Println("Received data:", value)
+func receiveCode(pin rpio.Pin) int {
+	code := 0
+
+	for i := 0; i < 32; i++ {
+		code <<= 1
+		if pin.Read() == rpio.High {
+			code |= 1
+		}
+		time.Sleep(time.Millisecond * 2)
+	}
+
+	return code
 }
--- tests/radio/send/main.go
+++ tests/radio/send/main.go
@@ -2,36 +2,55 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
 func main() {
-
-	// 初始化GPIO库
 	err := rpio.Open()
 	if err != nil {
-		fmt.Println("Failed to open GPIO:", err)
-		os.Exit(1)
+		fmt.Println(err)
+		return
 	}
 	defer rpio.Close()
 
-	// 设置发送引脚为输出模式
-	pin := rpio.Pin(22)
+	pin := rpio.Pin(20)
 	pin.Output()
 
-	// 发送无线电信号
+	sendCode := 2147483647
+	sendData(pin, sendCode)
+
+	fmt.Println("Data sent successfully!")
+}
+
+func sendData(pin rpio.Pin, code int) {
+	fmt.Printf("Sending data: %v", code)
+
 	for i := 0; i < 10; i++ {
-		pin.Low()
-		time.Sleep(time.Second)
-		pin.High()
-		time.Sleep(time.Millisecond * 200)
-		pin.Low()
-		time.Sleep(time.Millisecond * 100)
-		pin.High()
+		pin.Toggle()
 		time.Sleep(time.Millisecond * 200)
 	}
-	defer pin.Low()
+
+	// 转化为二进制数组
+	binaryData := make([]byte, 4)
+	binaryData[0] = byte(code >> 24)
+	binaryData[1] = byte(code >> 16)
+	binaryData[2] = byte(code >> 8)
+	binaryData[3] = byte(code)
+
+	for _, data := range binaryData {
+		for i := 0; i < 8; i++ {
+			if (data & 0x80) == 0x80 {
+				pin.High()
+			} else {
+				pin.Low()
+			}
+			data <<= 1
+			time.Sleep(time.Millisecond * 2)
+		}
+	}
+
+	pin.Low()
+	time.Sleep(time.Millisecond * 100)
 }
